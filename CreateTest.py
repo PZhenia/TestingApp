832bc83c9e1da93babbc584aea3d291e965a2e6b
lidasukhostavska <sukhostavska.lidiia@lll.kpi.ua>
2024-10-28 22:07:51 +0200

    CreateTest.py

--- CreateTest.py
+++ CreateTest.py
@@ -1,19 +1,47 @@
-"""Клас TestCreator:
-    Метод __ініціалізації__():
-        self.tests = []  // Список для зберігання тестів
+from tkinter import simpledialog, messagebox
+from test import Test  # Імпорт класу Test
 
-    Метод add_test(test_name, questions, test_type):
-        новий_test = {
-            'name': test_name,  // Назва тесту
-            'questions': questions,  // Список запитань
-            'type': test_type  // Тип тесту (тест або вікторина)
-        }
-        self.tests.append(новий_test)  // Додати тест до списку
-        self.save_tests()// Зберегти тести в бінарному файлі
+class TestCreate:
+    def __init__(self, manager):
+        self.manager = manager
 
-    Метод save_tests():
-        with open("tests.dat", "wb") as file:
-            pickle.dump(self.tests, file)
+    def create_test(self):
+        title = simpledialog.askstring("Create Test", "Enter test title:")
+        if title is None:
+            return  # Користувач натиснув Cancel
+        topic = simpledialog.askstring("Create Test", "Enter test topic:")
+        if topic is None:
+            return  # Користувач натиснув Cancel
+        questions = []
 
-    Метод load_tests(filename):
-        // Логіка для завантаження тестів з бінарного файлу
+        while True:
+            question = simpledialog.askstring("New Question", "Enter question (or leave blank to finish):")
+            if question is None:  # Користувач натиснув Cancel
+                return
+            if not question:
+                break
+
+            q_type = simpledialog.askstring("Question Type", "Enter type (single/multiple/open):")
+            if q_type is None:  # Користувач натиснув Cancel
+                return
+            options = []
+            answer = None
+
+            if q_type == "single" or q_type == "multiple":
+                options = simpledialog.askstring("Options", "Enter options separated by commas:").split(',')
+                if options is None:  # Користувач натиснув Cancel
+                    return
+                answer = simpledialog.askstring("Answer", "Enter correct answer(s) separated by commas:")
+                if answer is None:  # Користувач натиснув Cancel
+                    return
+            elif q_type == "open":
+                answer = simpledialog.askstring("Answer", "Enter correct answer:")
+                if answer is None:  # Користувач натиснув Cancel
+                    return
+
+            questions.append({"question": question, "type": q_type, "options": options, "answer": answer})
+
+        new_test = Test(title, questions, topic)
+        self.manager.save_test(new_test)
+        messagebox.showinfo("Success", "Test created successfully.")
+
