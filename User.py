2b1d2014678136eb87761382d614f2659e22f5a6
Zhenia <zheniapravdenko@gmail.com>
2024-11-26 21:04:34 +0200

    User class extension

--- User.py
+++ User.py
@@ -15,3 +15,37 @@ class UserProgress:
 
     def view_progress(self):
         return "\n".join([f"{test}: {scores}" for test, scores in self.test_attempts.items()])
+
+    def get_statistics(self):
+        total_tests = len(self.test_attempts)
+        average_score = (
+            sum(sum(scores) for scores in self.test_attempts.values()) / total_tests
+            if total_tests > 0 else 0
+        )
+        return f"Total Tests: {total_tests}, Average Score: {average_score:.2f}"
+
+class BaseUser:
+    def __init__(self, username):
+        self.username = username
+        self.progress = UserProgress()
+
+    def has_access_to_create_tests(self):
+        return False
+
+    def get_user_type(self):
+        return "Base User"
+
+class StandardUser(BaseUser):
+    def __init__(self, username, password):
+        super().__init__(username)
+        self.password = password
+
+    def has_access_to_create_tests(self):
+        return True
+
+    def get_user_type(self):
+        return "Standard User"
+
+class GuestUser(BaseUser):
+    def __init__(self, username="Guest"):
+        super().__init__(username)
